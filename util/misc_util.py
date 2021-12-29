c99e4be74b4d029ac068c9952293270e392650bd
Conner Lane <seltzerfish@gmail.com>
2021-12-29 13:53:27 -0800

    update market_closing_soon

--- util/misc_util.py
+++ util/misc_util.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 # returns true if the market will close within 10 minutes
 def market_closing_soon():
-    return (
-        int(datetime.utcnow().strftime("%H")) == 20
-        and int(datetime.utcnow().strftime("%M")) >= 50
-    )
+    if int(datetime.utcnow().strftime("%H")) == 21:
+        return True
+    if int(datetime.utcnow().strftime("%H")) == 20 and int(datetime.utcnow().strftime("%M")) >= 50:
+        return True
+    return False
