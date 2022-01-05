d611ef7c5fefeb6e6e263293120edaa35a2e774f
Conner Lane <seltzerfish@gmail.com>
2022-01-04 23:14:08 -0800

    manually check market hours bc i suspect something is wrong with alpaca's

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import strategy_list
 from trade_bot import TradeBot
 from time import sleep
-from util import alpaca_util
+from util import alpaca_util, misc_util
 
 if __name__ == "__main__":
     alpaca_util.clear_portfolio()
     try:
         trade_bot = TradeBot(strategy_list.GOOG_NEWS_TEST_STRATEGY)
         while True:
-            if alpaca_util.market_open():
+            if misc_util.within_market_hours() and alpaca_util.market_open():
                 trade_bot.update()
             else:
-                print("market closed. sleeping 60s.")
                 sleep(60)
     except (Exception, KeyboardInterrupt) as e:
         print("error encountered. clearing portfolio.")
--- util/misc_util.py
+++ util/misc_util.py
@@ -3,8 +3,26 @@ from datetime import datetime
 
 # returns true if the market will close within 10 minutes
 def market_closing_soon():
-    if int(datetime.utcnow().strftime("%H")) == 21:
+    hour = int(datetime.utcnow().strftime("%H"))
+    minute = int(datetime.utcnow().strftime("%M"))
+
+    if hour == 21:
         return True
-    if int(datetime.utcnow().strftime("%H")) == 20 and int(datetime.utcnow().strftime("%M")) >= 50:
+    if hour == 20 and minute >= 50:
         return True
     return False
+
+
+def within_market_hours():
+    day = int(datetime.utcnow().strftime("%w"))
+    hour = int(datetime.utcnow().strftime("%H"))
+    minute = int(datetime.utcnow().strftime("%M"))
+    if day == 0 or day == 6:
+        return False
+    if hour > 20:
+        return False
+    if hour < 14:
+        return False
+    if hour == 14 and minute < 30:
+        return False
+    return True
